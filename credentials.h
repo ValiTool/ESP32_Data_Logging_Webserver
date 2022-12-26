bc01ed0e4490ca3a1609c3419d86dabfcd6ef7f5
ValiTool <perriconelucas@gmail.com>
2022-12-26 12:30:50 -0300

    General renaming

--- credentials.h
+++ credentials.h
@@ -1,4 +1,4 @@
 // Change to your WiFi credentials
-const char* ssid     = "your ssid";
-const char* password = "your password";
+const char* ssid     = "AS100";
+const char* password = "3d13538907";
 
